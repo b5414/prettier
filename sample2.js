fac18209be2e1aaab20ecf2ea459ba5c9a3889a0
b5414 <mr.b5414@yandex.ru>
2024-11-15 12:31:37 +0800

    Update sample2.js

--- sample2.js
+++ sample2.js
@@ -11,7 +11,7 @@ const items = [
 const initEditorEvents = () => {
 	this.detachEditorEvents();
 
-	this._eventsDisposables.push(this._editor.onDidFocusEditorText(() => this.fireEvent('focus', this)), this._editor.onDidBlurEditorText(() => this.fireEvent('blur', this)), this._editor.onMouseUp(e => e.target.type === GUTTER_LINE_NUMBERS && this.fireEvent('lineNumberClick', e.target.position.lineNumber, e)), this._editor.onDidChangeModelContent(e => this.fireEvent('change', this, this._editor.getValue())), this._editor.onDidChangeModel(e => this.fireEvent('change', this, this._editor.getValue())));
+	this._eventsDisposables.push(this._editor.onDidFocusEditorText(() => this.fireEvent('focus', this)), this._editor.onDidBlurEditorText(() => this.fireEvent('blur', this)), this._editor.onMouseUp(e => e.target.type === AAAAAA_LINE_NUMBERS && this.fireEvent('lineNumberClick', e.target.position.lineNumber, e)), this._editor.onDidChangeModelContent(e => this.fireEvent('change', this, this._editor.getValue())), this._editor.onDidChangeModel(e => this.fireEvent('change', this, this._editor.getValue())));
 };
 
 // иииииииииии:
@@ -19,7 +19,7 @@ const initEditorEvents = () => {
 <div>
 	<Button
 		onClick={()=>{
-			window.open('htadsasdasdadsadsasdadsasdasdadsadsadsadsadsasdadsts2/-/settings/ci_cd', '_blank');
+			window.open('htadsasdasdadsadsasdadsasdasdadsadsadsadsadsasdadstssssssadsasdasdasdd', '_blank');
 		}}>Где
 	взять
 	личный
@@ -32,5 +32,48 @@ const initEditorEvents = () => {
 // иииииииииии:
 
 const superFunc = ()=>{
-	return {arr:[()=>[{superJopa:()=>{},'aaaaaaaaaaaaaaaa':1,'aaaaaaa5665aaaaaaaaa':1,'aaaaaaaaaaaa66aaaa':1,'aaaaaaaaaa88aaaaaa':1,'aaaaaaaaaaaa2aaaa':1,'aaaaaaaaaaaa33aaaa':1,'aaaaaaaaaa4aaaaaa':1,'aaaaaaaaaaaa5aaaa':1,}]]}
+	
+	
+	return {
+		clearList: function(){
+			Ext.create('Conf.view.base.Window', {
+				title: TEXT.get('web.CONF.AdmScrlbalbalbalbalbalabalbablablalbalbalbalbalbalabalbablablaarList.Title'),
+				width: 500,
+				height: 100,
+				closeAction: 'destroy',
+				items: [
+					{
+						xtype: 'sssssss',
+						items: [
+							
+							
+							Ext.create('Conf.view.base.DatetimeField', {
+								margin: '10 10 10 10',
+								itemId: 'asdasdasdasd',
+								fieldLabel: TEXT.get('web.CONF.AblallablablbalbalbalbalbalabalbablablablablablablablabateTo'),
+								labelWidth: 180,
+								labelPad: 10,
+							})],
+					},
+				],
+				bbar: {
+					items: [
+						'->',
+						{
+							xtype: 'button',
+							text: TEXT.get('web.CONF.lbalbalbalbalbalabalbablablaarList.ButtonClear'),
+							handler: function(btn){
+								const wnd = btn.up('window');
+								this.asdadssadasd(wnd.down('[itemId=asd]').getValue());
+								wnd.hide();
+							}.bind(this),
+						},
+					],
+				},
+			}).show();
+		},
+		
+		arr:[()=>[{supeaaaaa:()=>{},'aaaaaaaaaaaaaaaa':1,'aaaaaaa5665aaaaaaaaa':1,'aaaaaaaaaaaa66aaaa':1,'aaaaaaaaaa88aaaaaa':1,'aaaaaaaaaaaa2aaaa':1,'aaaaaaaaaaaa33aaaa':1,'aaaaaaaaaa4aaaaaa':1,'aaaaaaaaaaaa5aaaa':1,}]]}
 };
+
+
